37fa5c39986d0b4039706a8bc9f0987f2b3ee743
Aleh Maksimau <oleg.maximoff@gmail.com>
2016-03-06 20:26:22 +0300

    Debug for deposit_coin_address

--- app/models/worker/deposit_coin_address.rb
+++ app/models/worker/deposit_coin_address.rb
@@ -2,7 +2,7 @@ module Worker
   class DepositCoinAddress
 
     def process(payload, metadata, delivery_info)
-      payload.symbolize_keys!
+      #payload.symbolize_keys!
 
       payment_address = PaymentAddress.find payload[:payment_address_id]
       return if payment_address.address.present?
