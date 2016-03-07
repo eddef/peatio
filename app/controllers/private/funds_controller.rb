c977d1b6532d3aba1058cc63492014618fcd51d6
Aleh Maksimau <aleh.maksimau@gmail.com>
2016-03-07 14:30:18 +0300

    #4 - debugging mq+freecoind issue

--- app/controllers/private/funds_controller.rb
+++ app/controllers/private/funds_controller.rb
@@ -21,7 +21,7 @@ module Private
 
     def gen_address
       current_user.accounts.each do |account|
-        next if not account.currency_obj.coin?
+        next if not (account.currency_obj && account.currency_obj.coin?)
 
         if account.payment_addresses.blank?
           account.payment_addresses.create(currency: account.currency)
