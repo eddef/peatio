71dcbe64f24f8febf08029fbd3baa2ea7623fd8a
Aleh Maksimau <aleh.maksimau@gmail.com>
2016-03-07 14:25:59 +0300

    #4 - debugging mq+freecoind issue

--- app/models/worker/deposit_coin_address.rb
+++ app/models/worker/deposit_coin_address.rb
@@ -3,6 +3,7 @@ module Worker
 
     def process(payload, metadata, delivery_info)
       #payload.symbolize_keys!
+      byebug
       payment_address = PaymentAddress.find payload[:payment_address_id]
       return if payment_address.address.present?
 
