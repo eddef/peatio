33b97f928d9a5c15c7c943271258473783511dc0
Aleh Maksimau <aleh.maksimau@gmail.com>
2016-03-07 14:31:56 +0300

    #4 - debugging mq+freecoind issue

--- app/models/worker/deposit_coin_address.rb
+++ app/models/worker/deposit_coin_address.rb
@@ -2,7 +2,7 @@ module Worker
   class DepositCoinAddress
 
     def process(payload, metadata, delivery_info)
-      puts 'DepositCoinAddress worker fired!'
+      @logger.debug 'DepositCoinAddress worker fired!'
       #payload.symbolize_keys!
       payment_address = PaymentAddress.find payload[:payment_address_id]
       return if payment_address.address.present?
