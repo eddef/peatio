0a75819127496bdf0a0453e5cd8f91500b455ccd
Aleh Maksimau <aleh.maksimau@gmail.com>
2016-03-07 14:54:28 +0300

    #4 - debugging mq+freecoind issue

---
+++ lib/daemons/deposit_coin.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+ENV["RAILS_ENV"] ||= "development"
+
+root = File.expand_path(File.dirname(__FILE__))
+root = File.dirname(root) until File.exists?(File.join(root, 'config'))
+Dir.chdir(root)
+
+require File.join(root, "config", "environment")
+
+Rails.logger = @logger = Logger.new STDOUT
+
+
+$running = true
+Signal.trap("TERM") do
+  $running = false
+end
+
+while($running) do
+  workers.each do |worker|
+    begin
+      worker.run
+    rescue
+      Rails.logger.error "#{worker.class.name} failed to run: #{$!}"
+      Rails.logger.error $!.backtrace[0,20].join("\n")
+    end
+  end
+
+  sleep 30
+end
