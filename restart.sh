bc4a030fa8b6aa2c26cf908f30afce447fd7ed58
Aleh Maksimau <aleh.maksimau@gmail.com>
2016-03-07 16:24:50 +0300

    #4 - App restart script

--- restart.sh
+++ restart.sh
@@ -1,3 +1,2 @@
 sudo service nginx restart
-RAILS_ENV=production bundle exec ./bin/rake daemons:stop
-RAILS_ENV=production bundle exec ./bin/rake daemons:start
+RAILS_ENV=production bundle exec rake daemons:start
