9a415755a8f495ec1501518c39f7bf14e9b5ad71
Aleh Maksimau <oleg.maximoff@gmail.com>
2016-03-12 14:19:04 +0300

    Merge channels datasource into currencies.yml

--- app/models/deposit_channel.rb
+++ app/models/deposit_channel.rb
@@ -1,7 +1,14 @@
-class DepositChannel < ActiveYamlBase
+class DepositChannel < ActiveYaml::Base
   include Channelable
   include HashCurrencible
   include International
+
+  class << self
+    private
+    def load_file
+      Currency.all.map { |cu| cu.channels['deposit'] }
+    end
+  end
 
   def accounts
     bank_accounts.map {|i| OpenStruct.new(i) }
--- app/models/withdraw_channel.rb
+++ app/models/withdraw_channel.rb
@@ -1,7 +1,14 @@
-class WithdrawChannel < ActiveYamlBase
+class WithdrawChannel < ActiveYaml::Base
   include Channelable
   include HashCurrencible
   include International
+
+  class << self
+    private
+    def load_file
+      Currency.all.map { |cu| cu.channels['withdraw'] }
+    end
+  end
 
   def blacklist
     self[:blacklist]
