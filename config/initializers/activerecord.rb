0b4eb90698384479e56b0b8b42dbafe7bf1e03a8
Aleh Maksimau <oleg.maximoff@gmail.com>
2016-03-13 13:01:53 +0300

    Further refactoring

--- config/initializers/activerecord.rb
+++ config/initializers/activerecord.rb
@@ -5,3 +5,60 @@ module ActiveModel
 end
 
 ActiveRecord::Base.extend ActiveHash::Associations::ActiveRecordExtensions
+
+class Coindeposit < ::Deposit
+  include ::AasmAbsolutely
+  include ::Deposits::Coinable
+end
+
+class Bankdeposit < ::Deposit
+  include ::AasmAbsolutely
+  include ::Deposits::Bankable
+  include ::FundSourceable
+
+  def charge!(txid)
+    with_lock do
+      submit!
+      accept!
+      touch(:done_at)
+      update_attribute(:txid, txid)
+    end
+  end
+end
+
+class Coinwithdraw < ::Withdraw
+  include ::AasmAbsolutely
+  include ::Withdraws::Coinable
+  include ::FundSourceable
+end
+
+class Bankwithdraw < ::Withdraw
+  include ::AasmAbsolutely
+  include ::Withdraws::Bankable
+  include ::FundSourceable
+end
+
+
+
+
+
+Currency.all.each do |currency|
+  if currency.coin?
+
+    klass = Class.new Coindeposit
+    Deposits.const_set currency.key.capitalize, klass
+
+    klass = Class.new Coinwithdraw
+    Withdraws.const_set currency.key.capitalize, klass
+
+  else
+
+    klass = Class.new Bankdeposit
+    Deposits.const_set currency.key.capitalize, klass
+
+    klass = Class.new Bankwithdraw
+    Withdraws.const_set currency.key.capitalize, klass
+
+  end
+end
+
