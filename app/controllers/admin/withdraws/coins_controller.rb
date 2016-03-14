c64c2f9b4ca8c091a965775cfc2d9ad3caec7ddb
Aleh Maksimau <oleg.maximoff@gmail.com>
2016-03-14 08:15:55 +0300

    Further refactoring

--- app/controllers/admin/withdraws/coins_controller.rb
+++ app/controllers/admin/withdraws/coins_controller.rb
@@ -1,10 +1,29 @@
 module Admin
   module Withdraws
-    class CoinsController < ::Admin::Withdraws::BaseController
-      load_and_authorize_resource :class => '::Withdraws::Satoshi'
+    class CoinsController < ::Admin::BaseController
+      load_and_authorize_resource :class => "::Withdraws::#{ params[:type].capitalize }"
+
+      before_action :find_withdraw, only: [:show, :update, :destroy]
+
+      def channel
+        @channel ||= WithdrawChannel.find_by_key(self.controller_name.singularize)
+      end
+
+      def kls
+        channel.kls
+      end
+
+      def find_withdraw
+        w = channel.kls.find(params[:id])
+        self.instance_variable_set("@#{self.controller_name.singularize}", w)
+        raise "@#{ params[:type].capitalize.singularize }".inspect
+        if w.may_process? and (w.amount > w.account.locked)
+          flash[:alert] = 'TECH ERROR !!!!'
+          redirect_to action: :index
+        end
+      end
 
       def index
-        raise params.inspect
         start_at = DateTime.now.ago(60 * 60 * 24)
         @one_satoshis = @satoshis.with_aasm_state(:accepted).order("id DESC")
         @all_satoshis = @satoshis.without_aasm_state(:accepted).where('created_at > ?', start_at).order("id DESC")
