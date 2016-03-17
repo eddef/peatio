e54b1762debd807bb661e8efa98b9e19ad62189f
Aleh Maksimau <oleg.maximoff@gmail.com>
2016-03-17 18:56:05 +0300

    prod deploy configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,54 +1,36 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
+namespace :deploy do
+  on roles(:app) do
+    set :application, 'peatio'
+    set :repo_url, 'git@github.com:eddef/peatio.git'
 
-set :application, 'peatio'
-set :repo_url, 'git@github.com:eddef/peatio.git'
+    set :rvm_type, :user
+    set :rvm_ruby_version, '2.2.1'
+    set :rvm_custom_path, '~/.rvm'
 
-set :rvm_type, :user
-set :rvm_ruby_version, '2.2.1'
-set :rvm_custom_path, '~/.rvm'
+    set :branch, `git branch | grep "*" | sed "s/* //" | awk '{printf $0}'`
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, `git branch | grep "*" | sed "s/* //" | awk '{printf $0}'`
+    set :deploy_to, '~/peatio-cap'
 
-# Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/deploy/peatio-cap'
+    set :nginx_sites_available_path, "/etc/nginx/sites-available"
+    set :nginx_sites_enabled_path, "/etc/nginx/sites-enabled"
 
-set :nginx_sites_available_path, "/etc/nginx/sites-available"
-set :nginx_sites_enabled_path, "/etc/nginx/sites-enabled"
+    set :linked_files, fetch(:linked_files, []).push(
+        'config/database.yml',
+        'config/application.yml',
+        'config/currencies.yml',
+        'config/markets.yml',
+        'config/amqp.yml',
+        'config/banks.yml',
+        'puma.rb'
+    )
 
-# Default value for :scm is :git
-# set :scm, :git
 
-# Default value for :format is :pretty
-# set :format, :pretty
+    set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push(
-  'config/database.yml',
-  'config/application.yml',
-  'config/currencies.yml',
-  'config/markets.yml',
-  'config/amqp.yml',
-  'config/banks.yml',
-  'puma.rb'
-)
-
-# Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+  end
+end
 
 namespace :deploy do
   task :restart_daemons do
