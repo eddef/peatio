b6387be02d1595c68a73837315ae1c2656c66996
Aleh Maksimau <oleg.maximoff@gmail.com>
2016-03-17 19:00:35 +0300

    prod deploy configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,7 @@ namespace :deploy do
   on roles(:app) do
     set :application, 'peatio'
     set :repo_url, 'git@github.com:eddef/peatio.git'
+    set :repository, 'git@github.com:eddef/peatio.git'
 
     set :rvm_type, :user
     set :rvm_ruby_version, '2.2.1'
