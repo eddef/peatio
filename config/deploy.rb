ceb8de547667dbeb77f4be0c78367c14f93cf377
Aleh Maksimau <oleg.maximoff@gmail.com>
2016-03-17 21:47:48 +0300

    configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,23 +1,2 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
-#namespace :deploy do
-#  on roles(:app) do
-
-
-#  end
-#end
-
-=begin
-namespace :deploy do
-  task :restart_daemons do
-    `cd #{fetch(:deploy_to)}/current; /usr/bin/env rake daemons:stop daemons:start RAILS_ENV=#{fetch(:rails_env)}`
-  end
-end
-
-task :restart_daemons do
-  `cap #{fetch(:rails_env)} deploy:restart_daemons`
-end
-
-after :deploy, :restart_daemons
-
-=end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,11 +4,7 @@ set :rails_env, 'production'
 
 role :app, %w{exchanger@212.125.247.41}
 role :web, %w{exchanger@212.125.247.41}
-role :db,  %w{exchanger@212.125.247.42}, primary: true
-
-server '212.125.247.41', user: 'exchanger', roles: %w{web}, no_release: true
-server '212.125.247.41', user: 'exchanger', roles: %w{app}
-server '212.125.247.42', user: 'exchanger', roles: %w{db}
+# role :db,  %w{exchanger@212.125.247.42}, primary: true
 
 set :nginx_server_name, 'freiexchange.com'
 
@@ -23,7 +19,6 @@ set :deploy_to, '/home/exchanger/peatio-cap'
 
 set :nginx_sites_available_path, "/etc/nginx/sites-available"
 set :nginx_sites_enabled_path, "/etc/nginx/sites-enabled"
-
 
 set :linked_files, fetch(:linked_files, []).push(
     'config/database.yml',
