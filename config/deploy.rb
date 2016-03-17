ee78958cd73642d5acbdb11d07fec9f677742e40
Aleh Maksimau <oleg.maximoff@gmail.com>
2016-03-17 19:42:00 +0300

    configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,36 +1,13 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
-namespace :deploy do
-  on roles(:app) do
-    set :application, 'peatio'
-    set :repo_url, 'git@github.com:eddef/peatio.git'
+#namespace :deploy do
+#  on roles(:app) do
 
-    set :rvm_type, :user
-    set :rvm_ruby_version, '2.2.1'
-    set :rvm_custom_path, '~/.rvm'
 
-    #set :branch, `git branch | grep "*" | sed "s/* //" | awk '{printf $0}'`
+#  end
+#end
 
-    set :deploy_to, '~/peatio-cap'
-
-    set :nginx_sites_available_path, "/etc/nginx/sites-available"
-    set :nginx_sites_enabled_path, "/etc/nginx/sites-enabled"
-
-    set :linked_files, fetch(:linked_files, []).push(
-        'config/database.yml',
-        'config/application.yml',
-        'config/currencies.yml',
-        'config/markets.yml',
-        'config/amqp.yml',
-        'config/banks.yml',
-        'puma.rb'
-    )
-
-    set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
-  end
-end
-
+=begin
 namespace :deploy do
   task :restart_daemons do
     `cd #{fetch(:deploy_to)}/current; /usr/bin/env rake daemons:stop daemons:start RAILS_ENV=#{fetch(:rails_env)}`
@@ -43,13 +20,4 @@ end
 
 after :deploy, :restart_daemons
 
-namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-    end
-  end
-
-
-
-end
+=end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
+
 set :rails_env, 'production'
 
 role :app, %w{exchanger@212.125.247.41}
@@ -5,3 +6,32 @@ role :web, %w{exchanger@212.125.247.41}
 role :db,  %w{exchanger@212.125.247.42}, primary: true
 
 set :nginx_server_name, '212.125.247.41'
+
+namespace :deploy do
+  on role(:app) do
+    set :application, 'peatio'
+    set :repo_url, 'git@github.com:eddef/peatio.git'
+
+    set :rvm_type, :user
+    set :rvm_ruby_version, '2.2.1'
+    set :rvm_custom_path, '~/.rvm'
+
+    set :deploy_to, '~/peatio-cap'
+
+    set :nginx_sites_available_path, "/etc/nginx/sites-available"
+    set :nginx_sites_enabled_path, "/etc/nginx/sites-enabled"
+
+    set :linked_files, fetch(:linked_files, []).push(
+        'config/database.yml',
+        'config/application.yml',
+        'config/currencies.yml',
+        'config/markets.yml',
+        'config/amqp.yml',
+        'config/banks.yml',
+        'puma.rb'
+    )
+
+    set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+  end
+end
+
