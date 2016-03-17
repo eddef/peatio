b29bdb99ae130a3ad95c8c217962511fb664b9c8
Aleh Maksimau <oleg.maximoff@gmail.com>
2016-03-17 21:16:09 +0300

    configs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ set :rails_env, 'production'
 
 role :app, %w{exchanger@212.125.247.41}
 role :web, %w{exchanger@212.125.247.41}
-role :db,  %w{exchanger@212.125.247.42}, primary: true
+#role :db,  %w{exchanger@212.125.247.42}, primary: true
 
 set :nginx_server_name, 'freiexchange.com'
 
